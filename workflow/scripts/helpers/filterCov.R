69ee9454906941a7a0cddf09f3a580b40f69929d
Gab-23 <gabrieleoliveto26@gmail.com>
2025-11-03 16:06:11 +0100

    minor fix in make_beta_vals.R

--- workflow/scripts/helpers/filterCov.R
+++ workflow/scripts/helpers/filterCov.R
@@ -1,6 +1,6 @@
 filterCov <- function(counts, minCov, minSamples=90){
 	nb1 <- nrow(counts)
-	counts <- cov_df[which(apply(cov_df,1,minCov=minCov,minSamples=minSamples,function(x,minCov,minSamples){ sum(x>=minCov) })>=minSamples),] 
+	counts <- counts[which(apply(counts,1,minCov=minCov,minSamples=minSamples,function(x,minCov,minSamples){ sum(x>=minCov) })>=minSamples),] 
 	message(paste(nb1-nrow(counts),"rows out of",nb1,"were removed [COVERAGE]."))
 	return(counts)
 }
