30989b17549b0afe5e01572adb923b1e75ca63fd
Gab-23 <gabrieleoliveto26@gmail.com>
2026-01-23 09:32:49 +0100

    fixes

--- workflow/scripts/helpers/filterBetas.R
+++ workflow/scripts/helpers/filterBetas.R
@@ -1,4 +1,4 @@
-filterBetas <- function(counts, leftCount_beta=0.25, rightCount_beta=0.75, minSamples=90){
+filterBetas <- function(counts, leftCount_beta=0.5, rightCount_beta=0.5, minSamples=90){
 	nb1 <- nrow(counts)
 	left_CpGs <- counts[which(apply(counts,1,leftCount_beta=leftCount_beta,minSamples=minSamples,function(x,leftCount_beta,minSamples){ sum(x<=leftCount_beta) })>=minSamples),, drop = FALSE] 
 	right_CpGs <- counts[which(apply(counts,1,rightCount_beta=rightCount_beta,minSamples=minSamples,function(x,rightCount_beta,minSamples){ sum(x>=rightCount_beta) })>=minSamples),, drop = FALSE]
